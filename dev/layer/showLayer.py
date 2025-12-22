81bf6db9451cfb064d7910e5f1441ba1fe3f1fe8
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2025-12-22 22:38:25 +0100

    Create showLayer.py

---
+++ dev/layer/showLayer.py
@@ -0,0 +1,24 @@
+"""
+Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
+Version: 1.0
+Date: 22/12/25
+"""
+import rhinoscriptsyntax as rs
+
+__commandname__ = "showLayer"
+
+# RunCommand is the called when the user enters the command name in Rhino.
+# The command name is defined by the filname minus "_cmd.py"
+def RunCommand( is_interactive ):
+    layers = rs.LayerNames()
+    if layers:
+        layer = rs.CurrentLayer()
+        layer = rs.GetString("Layer to show", layer, layers)
+        if layer and rs.IsLayer(layer):
+            rs.LayerVisible(layer, True)
+        else:
+            print("This is not a layer")
+            return 1
+    return 0
+
+RunCommand(True)
