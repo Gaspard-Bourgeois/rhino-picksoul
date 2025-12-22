0b1116d123ba291e24193ac252d224c580d42794
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2025-12-22 14:12:20 +0100

    Update openPluginFolder.py

--- dev/utilities/openPluginFolder.py
+++ dev/utilities/openPluginFolder.py
@@ -1,3 +1,8 @@
+"""
+Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
+Version: 1.0
+Date: 22/12/25
+"""
 import os
 import rhinoscriptsyntax as rs
 path = "%AppData%/McNeel/Rhinoceros/7.0/Plug-ins/PythonPlugins/"
