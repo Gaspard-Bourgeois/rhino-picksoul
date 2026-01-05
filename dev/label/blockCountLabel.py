53432656637865dc3c76c8def9c7a3ee909d4239
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-05 15:42:39 +0100

    Update blockCountLabel.py

--- dev/label/blockCountLabel.py
+++ dev/label/blockCountLabel.py
@@ -1,15 +1,21 @@
+"""
+Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
+Version: 1.0
+Date: 05/01/26
+"""
 import rhinoscriptsyntax as rs
 
 def BlockCountLabels():
-    ids = rs.GetObjects("Sélectionnez les blocs pour compter leurs instances", rs.filter.instance)
+    ids = rs.GetObjects("Sélectionnez les blocs pour compter leurs instances", rs.filter.instance, preselect=True)
     if not ids: return
-
+    rs.UnselectAllObjects()
+    
     new_texts = []
     for id in ids:
-        name = rs.BlockInstanceName(id)
-        count = len(rs.BlockInstances(name)) # Compte total dans le doc
         point = rs.BlockInstanceInsertPoint(id)
-        txt_id = rs.AddText(str(count), point, height=1.0)
+        name = rs.BlockInstanceName(id)
+        formula = "%<BlockInstanceCount(\"" + str(name) + "\")>%"
+        txt_id = rs.AddText(formula, point, height=1.0)
         if txt_id: new_texts.append(txt_id)
     
     if new_texts: rs.SelectObjects(new_texts)
