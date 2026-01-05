3ab60f87f0601fb20e9ddf8f62bb01cb0d1bb9f7
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-05 16:01:16 +0100

    Update selectDuplicateNames.py

--- dev/selection/selectDuplicateNames.py
+++ dev/selection/selectDuplicateNames.py
@@ -1,26 +1,34 @@
+"""
+Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
+Version: 1.0
+Date: 05/01/26
+"""
 import rhinoscriptsyntax as rs
 
 def SelectDuplicateNames():
     selected = rs.SelectedObjects()
     if not selected: 
-        print("Veuillez sélectionner des objets d'abord.")
-        return
+        selected = rs.AllObjects()
 
     name_map = {}
     for obj in selected:
         # Récupère le nom (soit du bloc, soit de l'objet courbe)
-        name = rs.BlockInstanceName(obj) if rs.IsBlockInstance(obj) else rs.ObjectName(obj)
+        name = rs.ObjectName(obj)
         if name:
             if name not in name_map: name_map[name] = []
             name_map[name].append(obj)
 
     rs.UnselectAllObjects()
     to_select = []
+    count_map = 0
     for name in name_map:
         if len(name_map[name]) > 1: # S'il y a plus d'un objet avec ce nom
             to_select.extend(name_map[name])
+            count_map += 1
     
-    if to_select: rs.SelectObjects(to_select)
+    if to_select: 
+        print("{} objet(s) dans {} groupe(s) de nom multiples".format(len(to_select), count_map))
+        rs.SelectObjects(to_select)
     else: print("Aucun doublon de nom trouvé dans la sélection.")
 
 SelectDuplicateNames()
