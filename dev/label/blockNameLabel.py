86b6c950c276903ebd39816ab3b86ce805893d5b
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-05 15:42:52 +0100

    Update blockNameLabel.py

--- dev/label/blockNameLabel.py
+++ dev/label/blockNameLabel.py
@@ -1,14 +1,20 @@
+"""
+Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
+Version: 1.0
+Date: 05/01/26
+"""
 import rhinoscriptsyntax as rs
 
 def BlockNameLabels():
-    ids = rs.GetObjects("Sélectionnez les instances de bloc", rs.filter.instance)
+    ids = rs.GetObjects("Sélectionnez les instances de bloc", rs.filter.instance, preselect=True)
     if not ids: return
-
+    rs.UnselectAllObjects()
+    
     new_texts = []
     for id in ids:
         point = rs.BlockInstanceInsertPoint(id)
         # Formule de champ dynamique pour le nom du bloc
-        formula = "%<BlockName(\"" + str(id) + "\")>%"
+        formula = "%<BlockInstanceName(\"" + str(id) + "\")>%"
         txt_id = rs.AddText(formula, point, height=1.0)
         if txt_id: new_texts.append(txt_id)
     
