aae7e1b12c44b09bb0e37a02eef604ecb372d404
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-06 15:01:53 +0100

    Update rebuildPrograms.py

--- dev/IO/rebuildPrograms.py
+++ dev/IO/rebuildPrograms.py
@@ -2,127 +2,143 @@ import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 
 def analyze_and_rebuild_from_text():
-    # 1. Sélection intelligente
-    selected_objs = rs.GetObjects("Sélectionnez un programme ou un de ses éléments", preselect=True)
-    if not selected_objs: return
+    # 1. Sélection intelligente (Objet ou Calque)
+    selection = rs.GetObjects("Sélectionnez le programme, une courbe ou une pose", preselect=True)
+    if not selection: return
 
     program_instances = []
-    
-    for obj in selected_objs:
-        # Si c'est directement l'instance du programme
+    for obj in selection:
+        # Cas 1: Directement l'instance programme
         if rs.IsBlockInstance(obj) and rs.GetUserText(obj, "type") == "program":
             if obj not in program_instances: program_instances.append(obj)
-            continue
-        
-        # Sinon, on cherche si l'objet appartient à un calque de programme
-        layer = rs.ObjectLayer(obj)
-        # On remonte au calque parent qui contient l'instance "program"
-        parent_layer = layer.split("::")[0] if "::" in layer else layer
-        
-        # On cherche l'instance de bloc "program" sur ce calque
-        potential_progs = rs.ObjectsByLayer(parent_layer)
-        for p in potential_progs:
-            if rs.IsBlockInstance(p) and rs.GetUserText(p, "type") == "program":
-                if p not in program_instances: program_instances.append(p)
+        else:
+            # Cas 2: Objet dans le calque du programme
+            lyr = rs.ObjectLayer(obj)
+            root_lyr = lyr.split("::")[0]
+            objs_in_root = rs.ObjectsByLayer(root_lyr)
+            for o in objs_in_root:
+                if rs.IsBlockInstance(o) and rs.GetUserText(o, "type") == "program":
+                    if o not in program_instances: program_instances.append(o)
 
     if not program_instances:
-        rs.MessageBox("Aucun programme associé à la sélection n'a été trouvé.")
+        rs.MessageBox("Impossible de trouver le bloc 'program' associé.")
         return
 
     rs.EnableRedraw(False)
 
     for prog_inst in program_instances:
         main_lyr = rs.ObjectLayer(prog_inst)
-        traj_lyr_name = main_lyr + "::trajs_arcon_arcof"
-        se_lyr_name = main_lyr + "::start_end"
-        
-        # Collecte des données de reconstruction (Courbes -> Points)
-        keys = sorted([k for k in rs.GetUserText(prog_inst) if k.startswith("Crv_")], 
-                      key=lambda x: int(x.split("_")[1]))
-        
-        ordered_curve_origins = [rs.GetUserText(prog_inst, k) for k in keys]
-        
-        # On mappe les objets actuels par leur uuid_origin pour retrouver les copies
+        traj_lyr = main_lyr + "::trajs_arcon_arcof"
+        se_lyr = main_lyr + "::start_end"
+
+        # 2. Récupération de l'ordre des courbes via le bloc program
+        crv_keys = sorted([k for k in rs.GetUserText(prog_inst) if k.startswith("Crv_")], key=lambda x: int(x.split("_")[1]))
+        ordered_uuids = [rs.GetUserText(prog_inst, k) for k in crv_keys]
+
+        # 3. Collecte de la géométrie actuelle (incluant copies)
         all_objs = rs.AllObjects()
-        obj_map = {}
+        obj_map = {} # { uuid_origin : [list of objects] }
         for o in all_objs:
             u_orig = rs.GetUserText(o, "uuid_origin")
             if u_orig:
                 if u_orig not in obj_map: obj_map[u_orig] = []
                 obj_map[u_orig].append(o)
 
-        full_sequence = [] # Liste de {'pos', 'arcon'}
+        sequence_data = [] # {pos, meta_dict}
         
-        for u_crv_orig in ordered_curve_origins:
-            if u_crv_orig not in obj_map: continue
+        for u_orig in ordered_uuids:
+            if u_orig not in obj_map: continue
             
-            # On prend la dernière instance de la courbe (la copie la plus récente)
-            current_crv = obj_map[u_crv_orig][-1] 
-            is_arcon = rs.ObjectColor(current_crv) == (255,0,0) or "ARCON" in (rs.ObjectName(current_crv) or "")
+            # On prend la dernière instance trouvée (si copie, c'est la plus récente)
+            target_crv = obj_map[u_orig][-1]
+            verts = rs.PolylineVertices(target_crv)
+            if not verts: continue
             
-            vertices = rs.PolylineVertices(current_crv)
-            if not vertices: continue
+            # On récupère les UserStrings de la courbe originale ou du bloc Pose d'origine
+            # pour restaurer V, PL, BC etc.
+            arc_state = rs.GetUserText(target_crv, "ArcState") or "ARCOF"
             
-            # Pour chaque segment, on ajoute les points. 
-            # Attention : pour éviter les doublons aux jointures, on gère l'overlap
-            start_idx = 1 if len(full_sequence) > 0 else 0
-            for i in range(start_idx, len(vertices)):
-                full_sequence.append({'pos': vertices[i], 'arcon': is_arcon})
+            for i in range(len(verts)):
+                # Éviter les doublons aux points de jonction des courbes
+                if i == 0 and len(sequence_data) > 0: continue
+                
+                # Récupérer les métadonnées de la pose d'origine associée à ce point
+                u_pose_orig = rs.GetUserText(target_crv, "UUID_{}".format(i))
+                meta = {}
+                if u_pose_orig and u_pose_orig in obj_map:
+                    pose_ref = obj_map[u_pose_orig][0]
+                    keys = ["ID_C", "ID_BC", "Type", "V_Type", "V_Val", "PL"]
+                    for k in keys: meta[k] = rs.GetUserText(pose_ref, k)
+                
+                sequence_data.append({
+                    'pos': verts[i],
+                    'arc': arc_state,
+                    'meta': meta
+                })
 
-        if not full_sequence: continue
-
-        # --- NETTOYAGE ET RECONSTRUCTION ---
+        # 4. Nettoyage In-Place
+        # Supprimer les poses existantes sur le calque principal
+        for o in rs.ObjectsByLayer(main_lyr):
+            if rs.IsBlockInstance(o) and rs.BlockInstanceName(o) == "Pose": rs.DeleteObject(o)
         
-        # 1. Nettoyer les anciennes poses et trajectoires
-        old_poses = rs.ObjectsByLayer(main_lyr)
-        for op in old_poses:
-            if rs.IsBlockInstance(op) and rs.BlockInstanceName(op) == "Pose": rs.DeleteObject(op)
-            
-        if rs.IsLayer(traj_lyr_name):
-            rs.DeleteObjects(rs.ObjectsByLayer(traj_lyr_name))
-        else:
-            rs.AddLayer("trajs_arcon_arcof", parent=main_lyr)
+        # Supprimer courbes et start/end
+        if rs.IsLayer(traj_lyr): rs.DeleteObjects(rs.ObjectsByLayer(traj_lyr))
+        if rs.IsLayer(se_lyr): rs.DeleteObjects(rs.ObjectsByLayer(se_lyr))
 
-        if rs.IsLayer(se_lyr_name):
-            rs.DeleteObjects(rs.ObjectsByLayer(se_lyr_name))
-        else:
-            rs.AddLayer("start_end", parent=main_lyr)
-
-        # 2. Nouvelles Poses
+        # 5. Reconstruction
+        new_poses_uuids = []
         rs.CurrentLayer(main_lyr)
-        for i, data in enumerate(full_sequence):
-            new_p = rs.InsertBlock("Pose", data['pos'])
-            rs.ObjectName(new_p, str(i))
-            rs.SetUserText(new_p, "uuid_origin", str(new_p))
+        for i, data in enumerate(sequence_data):
+            p_id = rs.InsertBlock("Pose", data['pos'])
+            rs.ObjectName(p_id, str(i))
+            rs.SetUserText(p_id, "uuid_origin", str(p_id))
+            # Restaurer les métadonnées
+            for k, v in data['meta'].items(): 
+                if v: rs.SetUserText(p_id, k, v)
+            rs.SetUserText(p_id, "ArcState", data['arc'])
+            new_poses_uuids.append(str(p_id))
 
-        # 3. Nouvelles Trajectoires
-        rs.CurrentLayer(traj_lyr_name)
-        if len(full_sequence) > 1:
-            temp_pts = [full_sequence[0]['pos']]
-            last_state = full_sequence[0]['arcon']
-            
-            def build_pl(pts, state):
+        # Reconstruction des courbes avec noms corrects
+        if not rs.IsLayer(traj_lyr): rs.AddLayer("trajs_arcon_arcof", parent=main_lyr)
+        rs.CurrentLayer(traj_lyr)
+        
+        if len(sequence_data) > 1:
+            temp_pts = [sequence_data[0]['pos']]
+            temp_uuids = [new_poses_uuids[0]]
+            last_arc = sequence_data[0]['arc']
+            start_idx = 0
+
+            def build(pts, arc, s_i, e_i, uids):
                 if len(pts) < 2: return
                 pid = rs.AddPolyline(pts)
-                rs.ObjectColor(pid, (255,0,0) if state else (150,150,150))
+                rs.ObjectName(pid, "{} {}-{}".format(arc, s_i, e_i))
+                rs.ObjectColor(pid, (255,0,0) if arc == "ARCON" else (150,150,150))
                 rs.SetUserText(pid, "uuid_origin", str(pid))
+                rs.SetUserText(pid, "ArcState", arc)
+                for j, u in enumerate(uids):
+                    rs.SetUserText(pid, "UUID_{}".format(j), u)
+                    rs.SetUserText(pid, "Pt_{}".format(j), str(s_i + j))
 
-            for i in range(1, len(full_sequence)):
-                if full_sequence[i]['arcon'] != last_state:
-                    build_pl(temp_pts, last_state)
-                    temp_pts = [full_sequence[i-1]['pos'], full_sequence[i]['pos']]
-                    last_state = full_sequence[i]['arcon']
+            for i in range(1, len(sequence_data)):
+                if sequence_data[i]['arc'] != last_arc:
+                    build(temp_pts, last_arc, start_idx, i-1, temp_uuids)
+                    temp_pts = [sequence_data[i-1]['pos'], sequence_data[i]['pos']]
+                    temp_uuids = [new_poses_uuids[i-1], new_poses_uuids[i]]
+                    start_idx = i - 1
+                    last_arc = sequence_data[i]['arc']
                 else:
-                    temp_pts.append(full_sequence[i]['pos'])
-            build_pl(temp_pts, last_state)
+                    temp_pts.append(sequence_data[i]['pos'])
+                    temp_uuids.append(new_poses_uuids[i])
+            build(temp_pts, last_arc, start_idx, len(sequence_data)-1, temp_uuids)
 
-        # 4. Start & End
-        rs.CurrentLayer(se_lyr_name)
-        rs.InsertBlock("Start", full_sequence[0]['pos'])
-        rs.InsertBlock("End", full_sequence[-1]['pos'])
+        # Start/End
+        if not rs.IsLayer(se_lyr): rs.AddLayer("start_end", parent=main_lyr)
+        rs.CurrentLayer(se_lyr)
+        rs.InsertBlock("Start", sequence_data[0]['pos'])
+        rs.InsertBlock("End", sequence_data[-1]['pos'])
 
     rs.EnableRedraw(True)
-    rs.MessageBox("Reconstruction terminée.")
+    rs.MessageBox("Reconstruction terminée avec succès (Métadonnées préservées).")
 
 if __name__ == "__main__":
     analyze_and_rebuild_from_text()
