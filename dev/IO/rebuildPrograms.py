48e7b6e6afa22c8e61f75041eb964431bddf0573
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-08 17:12:57 +0100

    Update rebuildPrograms.py

--- dev/IO/rebuildPrograms.py
+++ dev/IO/rebuildPrograms.py
@@ -27,7 +27,8 @@ def get_pose_copies(origin_uuid, all_pose_instances, selected_ids=None):
         if str(inst) != origin_uuid:
             if rs.GetUserText(inst, "uuid_origin") == origin_uuid:
                 if selected_ids:
-                    if str(inst) in selected_ids: copies.append(inst)
+                    if str(inst) in selected_ids: 
+                        copies.append(inst)
                 else:
                     copies.append(inst)
     return copies
@@ -60,6 +61,7 @@ def rebuild_trajectories():
     
     for prog_id in target_programs:
         prog_layer = rs.ObjectLayer(prog_id)
+        all_poses_in_prog = [p for p in all_poses_in_doc if rs.ObjectLayer(p) == prog_layer]
         print("Programme : " + prog_layer)
         
         # --- 2. RECUPERATION DES COURBES (Originales + Copies) ---
@@ -69,31 +71,40 @@ def rebuild_trajectories():
             # On cherche Crv_0000, Crv_0001... (format souhaité)
             # Mais on checke aussi l'ancien format Crv_0 pour compatibilité ascendante
             key_new = "Crv_{:04d}".format(i)
-            key_old = "Crv_{}".format(i)
             
             u = rs.GetUserText(prog_id, key_new)
-            if not u: u = rs.GetUserText(prog_id, key_old)
             
             if not u: break
             if rs.IsObject(u): original_crv_uuids.append(u)
             i += 1
+        
         
         # Identification des copies de courbes
         all_curves = rs.ObjectsByType(4) or []
         curve_copies = []
         for c_id in all_curves:
             origin = rs.GetUserText(c_id, "uuid_origin")
+            # print(str(c_id))
             if origin in original_crv_uuids and str(c_id) != origin:
                 if selected:
-                    if str(c_id) in selected_ids_str: curve_copies.append(c_id)
+                    if str(c_id) in selected_ids_str: 
+                        curve_copies.append(c_id)
                 else:
                     curve_copies.append(c_id)
 
+        
+        rs.UnselectAllObjects()
+        rs.SelectObjects(curve_copies)
+        rs.EnableRedraw(True)
+
         # Choix position courbes copiées
-        insert_mode_crv = "After"
+        insert_mode_crv = "Après"
         if curve_copies:
             res = rs.ListBox(["Avant", "Après"], "Position des COPIES DE COURBES ?", "Ordre Courbes")
             if res: insert_mode_crv = res
+        
+        rs.EnableRedraw(False)
+
 
         # Construction de la liste ORDONNÉE des courbes
         ordered_curve_list = []
@@ -107,19 +118,38 @@ def rebuild_trajectories():
                 ordered_curve_list.extend(relevant_copies)
 
         # Choix position poses copiées (une fois globalement)
-        pose_insert_mode = "After"
-        # Check rapide s'il y a des copies
-        has_pose_copies = any(rs.GetUserText(p, "uuid_origin") and rs.GetUserText(p, "uuid_origin") != str(p) for p in all_poses_in_doc)
-        if has_pose_copies:
+        pose_insert_mode = "Après"
+        # Check s'il y a des copies
+        pose_copies = []
+        for p in all_poses_in_prog:
+            origin = rs.GetUserText(p, "uuid_origin")
+            if str(p) != origin:
+                if selected:
+                    if str(p) in selected_ids_str: 
+                        pose_copies.append(p)
+                else:
+                    pose_copies.append(p)
+        
+        
+        rs.UnselectAllObjects()
+        rs.SelectObjects(pose_copies)
+        rs.EnableRedraw(True)
+        
+        if pose_copies:
             res_p = rs.ListBox(["Avant", "Après"], "Position des COPIES DE POSES ?", "Ordre Poses")
-            if res_p: pose_insert_mode = res_p
+            if res_p: 
+                pose_insert_mode = res_p
+
+        rs.EnableRedraw(False)
 
         # --- 3. RECONSTRUCTION LINEAIRE DES POSES ---
-        final_pose_objects = [] # Liste plate de tous les UUIDs de poses finaux
+        all_pose_objects = [] # Liste plate de tous les UUIDs de poses finaux
         
-        for crv_id in ordered_curve_list:
-            is_copy_crv = (rs.GetUserText(crv_id, "uuid_origin") != str(crv_id))
-            ref_crv = rs.GetUserText(crv_id, "uuid_origin") if is_copy_crv else str(crv_id)
+        is_copy_crv = False
+        for c_idx, crv_id in enumerate(ordered_curve_list):
+            is_prev_copy_crv = is_copy_crv
+            ref_crv = str(rs.GetUserText(crv_id, "uuid_origin"))
+            is_copy_crv = (ref_crv != str(crv_id))
             
             # Récupération des poses stockées dans la courbe de référence
             # On supporte Pt_0000 et Pt_0
@@ -127,8 +157,7 @@ def rebuild_trajectories():
             p_idx = 0
             while True:
                 # Essai format 000X puis X
-                p_uuid = rs.GetUserText(ref_crv, "UUID_{:04d}".format(p_idx))
-                if not p_uuid: p_uuid = rs.GetUserText(ref_crv, "UUID_{}".format(p_idx))
+                p_uuid = rs.GetUserText(str(crv_id), "UUID_{:04d}".format(p_idx))
                 
                 if not p_uuid: break
                 poses_in_curve.append(p_uuid)
@@ -136,20 +165,36 @@ def rebuild_trajectories():
             
             if not is_copy_crv:
                 # CAS A : COURBE ORIGINALE (Existante)
-                for p_uuid in poses_in_curve:
+                if is_prev_copy_crv and rs.GetUserText(poses_in_curve[-1],"State") == "ARCOF":
+                    # En mode courbe après, on saute le premier point des courbes suivants les copies de type ARCOF
+                    poses_in_curve.pop(0)
+                        
+                for j, p_uuid in enumerate(poses_in_curve):
                     if not rs.IsObject(p_uuid): continue
                     
                     # Gestion des copies de poses (doublons)
-                    copies_of_this_pose = get_pose_copies(p_uuid, all_poses_in_doc, selected_ids_str)
+                    copies_of_this_pose = get_pose_copies(p_uuid, all_poses_in_prog, selected_ids_str)
                     
                     if pose_insert_mode == "Avant":
-                        final_pose_objects.extend(copies_of_this_pose)
-                        final_pose_objects.append(p_uuid)
+                        all_pose_objects.extend(copies_of_this_pose)
+                        all_pose_objects.append(p_uuid)
                     else:
-                        final_pose_objects.append(p_uuid)
-                        final_pose_objects.extend(copies_of_this_pose)
+                        all_pose_objects.append(p_uuid)
+                        all_pose_objects.extend(copies_of_this_pose)
             else:
                 # CAS B : COURBE COPIÉE (Nouvelle trajectoire)
+                # on va directement au points de transition si la courbe est de la transition
+                if insert_mode_crv == "Après" and rs.GetUserText(poses_in_curve[-1],"State") == "ARCOF":
+                    if all_pose_objects and ref_crv != original_crv_uuids[-1]:
+                        # Tout le temps sauf première et dernière courbe
+                        p_pop = all_pose_objects.pop()
+                # on va directement au points copiés si la courbe est du process
+                if insert_mode_crv == "Avant" and rs.GetUserText(poses_in_curve[-1],"State") == "ARCON":
+                    if all_pose_objects:
+                        p_pop = all_pose_objects.pop()
+                
+
+
                 # Création physique de nouvelles poses sur les sommets
                 pts = rs.PolylineVertices(crv_id)
                 # On ignore le dernier point si polyligne fermée identiquement au premier (rare en robotique mais possible)
@@ -166,14 +211,24 @@ def rebuild_trajectories():
                         curr_pt = rs.BlockInstanceInsertPoint(new_pose)
                         translation = pts[k] - curr_pt
                         rs.MoveObject(new_pose, translation)
+                        if rs.GetUserText(poses_in_curve[-1],"State") == "ARCOF":
+                            rs.SetUserText(new_pose, "State", "ARCOF")
                         # 3. Ajouter à la liste finale
-                        final_pose_objects.append(new_pose)
-
+                        all_pose_objects.append(new_pose)
+            print(all_pose_objects)
+            
+        # --- SUPPRESSION DES DOUBLONS ---
+        prev_p = None
+        for i, p in enumerate(all_pose_objects):
+            if p == prev_p:
+                all_pose_objects.pop(i)
+            prev_p = p
+        
         # --- 4. MISE A JOUR DES POSES (Renommage & UUID Origin) ---
         pose_data_list = []
         
         # C'est ici qu'on assure la continuité : idx va de 0 à N sans interruption
-        for idx, p_id in enumerate(final_pose_objects):
+        for idx, p_id in enumerate(all_pose_objects):
             if not rs.IsObject(p_id): continue
             
             # Format Nom : 0000, 0001, etc.
@@ -199,17 +254,14 @@ def rebuild_trajectories():
 
         # --- 5. RECONSTRUCTION GEOMETRIQUE DES COURBES ---
         # Préparation Layer
-        parent_lyr = rs.ParentLayer(prog_layer)
         traj_lyr_name = "trajs_arcon_arcof"
-        full_traj_lyr = (parent_lyr + "::" + traj_lyr_name) if parent_lyr else traj_lyr_name
+        full_traj_lyr = (prog_layer + "::" + traj_lyr_name)
+        print(full_traj_lyr)
         
         if not rs.IsLayer(full_traj_lyr): 
             # Fallback simple
-            if rs.IsLayer(prog_layer + "::" + traj_lyr_name):
-                 full_traj_lyr = prog_layer + "::" + traj_lyr_name
-            else:
+            if not rs.IsLayer(prog_layer + "::" + traj_lyr_name):
                  rs.AddLayer(traj_lyr_name, parent=prog_layer)
-                 full_traj_lyr = prog_layer + "::" + traj_lyr_name
                  
         rs.CurrentLayer(full_traj_lyr)
         
@@ -262,15 +314,18 @@ def rebuild_trajectories():
 
         # --- 6. MISE A JOUR BLOC PROGRAMME ---
         # Nettoyage clés
-        keys = rs.GetUserText(prog_id)
+        new_prog = rs.CopyObject(prog_id)
+        keys = rs.GetUserText(new_prog)
         if keys:
             for k in keys:
-                if k.startswith("Crv_"): rs.SetUserText(prog_id, k, "")
+                if k.startswith("Crv_"): rs.SetUserText(new_prog, k, "")
         
         # Ecriture format Crv_000X
         for i, u in enumerate(new_crv_uuids):
-            rs.SetUserText(prog_id, "Crv_{:04d}".format(i), u)
+            rs.SetUserText(new_prog, "Crv_{:04d}".format(i), u)
             
+        rs.DeleteObject(prog_id)
+        
         print("Rebuild termine. {} poses, {} courbes.".format(len(pose_data_list), len(new_crv_uuids)))
 
     rs.EnableRedraw(True)
