dee3c1d4a810bb552d283a427d51f6db53c23551
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-09 10:16:57 +0100

    Update copyBlockColor.py

--- dev/block/copyBlockColor.py
+++ dev/block/copyBlockColor.py
@@ -1,64 +1,81 @@
-"""
-Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
-Version: 1.0
-Date: 09/01/26
-"""
 import rhinoscriptsyntax as rs
 
-
 def copyBlockColor():
+    # 1. Sélections
+    ids_dst = rs.GetObjects("Sélectionnez les instances de bloc DESTINATION", 4096, preselect=True)
+    if not ids_dst: return
     
-    ids_dst = rs.GetObjects("Select block instances destination", 4096, preselect=True)
-    if not ids_dst:return
+    id_src = rs.GetObject("Sélectionnez l'instance de bloc SOURCE", 4096, preselect=False)
+    if not id_src: return
     
-    ids_src = rs.GetObject("Select block instances source", 4096, preselect=False)
-    if not ids_src:return
+    # Récupérer les noms des définitions de blocs
+    # On utilise un set pour éviter de traiter 10 fois le même bloc si on a sélectionné 10 instances
+    src_def_name = rs.BlockInstanceName(id_src)
+    dst_def_names = list(set([rs.BlockInstanceName(i) for i in ids_dst]))
     
-    src_names = list(set([rs.BlockInstanceName(id) for id in [ids_src]]))
-    dst_names = list(set([rs.BlockInstanceName(id) for id in ids_dst]))
-    
-    
-    
+    # Dictionnaires pour l'affichage (cosmétique)
     dict_color_src = {
-    0 : 'Color from layer',
-    1 : 'Color from object',
-    2 : 'Color from material',
-    3 : 'Color from parent'
+        0 : 'Color from layer',
+        1 : 'Color from object',
+        2 : 'Color from material',
+        3 : 'Color from parent'
     }
 
-    def BlockDrill(names, _type="src"):
-        global color_src
-        global color
-        while True:
-            if len (names) > 0 :
-                name = names.pop()
-            else: break
-            
-            done.append(name)
-            temp = rs.BlockObjects(name)
-            if _type == "src":
-                color = rs.ObjectColor(temp)
-                color_src = rs.ObjectColorSource(temp)
-                
-                print("Couleur d'affichage copiee :", dict_color_src[color_src])
-                if color_src == 1:
-                    print("Couleur copiee :", color)
-                    return
-            else: 
-                rs.ObjectColorSource(temp, color_src)
-                if color_src == 1:
-                    rs.ObjectColor(temp, color)
-            for tempId in temp:
-                if rs.IsBlockInstance(tempId):
-                    tempName = rs.BlockInstanceName(tempId)
-                    if tempName not in names and tempName not in done:
-                        names.append(tempName)
-                        BlockDrill(names)
-    done = []      
-    BlockDrill(src_names, _type="src")
+    # ---------------------------------------------------------
+    # ÉTAPE 1 : Analyser le bloc SOURCE (Récupérer une liste de styles)
+    # ---------------------------------------------------------
+    src_objects = rs.BlockObjects(src_def_name)
+    src_styles = []
 
-    done = []
-    BlockDrill(dst_names, _type= "dst")
+    print("--- Analyse de la source ---")
+    for obj in src_objects:
+        # On stocke les propriétés de chaque objet du bloc source dans un dictionnaire
+        style = {
+            'source': rs.ObjectColorSource(obj),
+            'color': rs.ObjectColor(obj)
+        }
+        src_styles.append(style)
+        
+        # Debug info
+        s_name = dict_color_src.get(style['source'], "Inconnu")
+        print("Obj Source recupere : {} | {}".format(s_name, style['color']))
+
+    if not src_styles:
+        print("Erreur : Le bloc source est vide.")
+        return
+
+    # ---------------------------------------------------------
+    # ÉTAPE 2 : Appliquer aux blocs DESTINATION
+    # ---------------------------------------------------------
+    rs.EnableRedraw(False) # Optimisation de vitesse
     
+    for dst_name in dst_def_names:
+        dst_objects = rs.BlockObjects(dst_name)
+        
+        if not dst_objects:
+            continue
+
+        # Boucle sur les objets de la destination
+        for i, dst_obj in enumerate(dst_objects):
+            
+            # --- C'EST ICI QUE LA MAGIE OPÈRE (Boucle intelligente) ---
+            # Si destination a plus d'objets que source, on utilise le modulo (%)
+            # pour revenir au début de la liste source.
+            # Ex: si src_styles a 2 éléments, l'index 2 devient 0, l'index 3 devient 1, etc.
+            src_index = i % len(src_styles)
+            
+            target_style = src_styles[src_index]
+            
+            # Application des modifications
+            rs.ObjectColorSource(dst_obj, target_style['source'])
+            
+            # On applique la couleur seulement si la source n'est pas "By Layer" ou "By Parent" 
+            # (Bien que techniquement on peut l'appliquer tout le temps, c'est plus propre ainsi)
+            if target_style['source'] == 1: # 1 = Color from Object
+                rs.ObjectColor(dst_obj, target_style['color'])
+
+    rs.EnableRedraw(True)
+    print("Mise a jour terminee sur {} definitions de blocs.".format(len(dst_def_names)))
+
 if __name__ == "__main__": 
     copyBlockColor()
