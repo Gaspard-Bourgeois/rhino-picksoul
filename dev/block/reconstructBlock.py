8cfa6a31ef821bc2eb26a375dc722e12dca408cb
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-01-31 20:08:37 +0100

    Update reconstructBlock.py

--- dev/block/reconstructBlock.py
+++ dev/block/reconstructBlock.py
@@ -1,4 +1,6 @@
 import rhinoscriptsyntax as rs
+import math
+import Rhino
 
 def rebuild_reciproque():
     # 1. Sélection des objets
@@ -8,6 +10,7 @@ def rebuild_reciproque():
     origin_obj = None
     block_name = None
     xform = None
+    block_name_list = rs.BlockNames()
 
     # 2. Recherche de l'objet origine via la clé
     for obj in initial_objs:
@@ -23,23 +26,35 @@ def rebuild_reciproque():
     if not origin_obj:
         ref_id = rs.GetObject("Origine non trouvée. Référence ou [Entrée] pour Monde")
         if ref_id:
-            # On utilise le nom de l'objet (ObjectName) et non celui de la définition de bloc
-            block_name = rs.ObjectName(ref_id)
-            if not block_name and rs.IsBlockInstance(ref_id):
-                block_name = rs.BlockInstanceName(ref_id)
+            block_name = rs.BlockInstanceName(ref_id)
+          
             
-            # Si toujours pas de nom, on demande
-            if not block_name:
-                block_name = rs.GetString("Nom du bloc", "NouveauBloc")
-                
-            xform = rs.BlockInstanceXform(ref_id) if rs.IsBlockInstance(ref_id) else rs.XformRotation(0, [0,0,1], [0,0,0])
+            if rs.IsBlockInstance(ref_id):
+                xform = rs.BlockInstanceXform(ref_id) 
+            else:
+                box = rs.BoundingBox(ref_id)
+                center = math.mean(box)
+                xform = Rhino.Geometry.Transform.Translation(center-[0,0,0])
         else:
-            block_name = rs.GetString("Nom du bloc", "NouveauBloc")
+            block_name = "NouveauBloc"
             xform = rs.XformIdentity()
+
+        if block_name[-5:].lower() == "_base":
+            block_name = block_name[:-5]
+        if block_name[-2:].isdigit() and block_name[-3] == "_":
+            block_name = block_name[:-3]
+        
+        free_block_name = block_name
+        for i in range(100):
+            if free_block_name not in block_name_list:
+                break
+            free_block_name =  "{}_{:02d}".format(block_name, i)
+        block_name = free_block_name
     
     if not block_name: return
 
     # 4. Insertion préalable pour comparaison visuelle
+    confirm = "Oui"
     temp_instance = None
     if rs.IsBlock(block_name):
         temp_instance = rs.InsertBlock(block_name, [0,0,0])
@@ -48,11 +63,8 @@ def rebuild_reciproque():
         rs.SelectObject(temp_instance)
         
         msg = "Le bloc '{}' existe déjà. Mettre à jour sa définition et remplacer les objets ?".format(block_name)
-    else:
-        msg = "Le bloc '{}' n'existe pas. Créer la définition et remplacer les objets ?".format(block_name)
-
-    # 5. Demande à l'utilisateur
-    confirm = rs.GetString(msg, "Oui", ["Oui", "Non"])
+        # 5. Demande à l'utilisateur
+        confirm = rs.GetString(msg, "Oui", ["Oui", "Non"])
     
     if confirm == "Oui":
         # Préparation de la géométrie (Transformation inverse pour le repère local 0,0,0)
@@ -61,7 +73,7 @@ def rebuild_reciproque():
         new_geometries = []
         for o in initial_objs:
             # On exclut l'objet "Pose" (trièdre) de la nouvelle définition pour ne pas polluer le bloc
-            if rs.IsBlockInstance(o) and rs.BlockInstanceName(o) == "Pose":
+            if rs.IsBlockInstance(o) and rs.BlockInstanceName(o) == "Pose" and rs.GetUserText(o, "OriginalBlockName"):
                 continue
             
             copy = rs.CopyObject(o)
@@ -70,6 +82,7 @@ def rebuild_reciproque():
 
         # Mise à jour ou création de la définition
         rs.AddBlock(new_geometries, [0,0,0], block_name, delete_input=True)
+        rs.DeleteObjects(new_geometries)
         
         # Si on avait inséré une instance temporaire, elle est déjà à jour (Rhino met à jour les instances)
         # Sinon, on en insère une nouvelle
@@ -82,15 +95,8 @@ def rebuild_reciproque():
         
         rs.UnselectAllObjects()
         rs.SelectObject(temp_instance)
-        print("Bloc '{}' reconstruit avec succès.".format(block_name))
+        print("Bloc '{}' (re)construit avec succès.".format(block_name))
         
-    else:
-        # Si Non : on nettoie l'instance temporaire si elle a été créée
-        if temp_instance:
-            rs.DeleteObject(temp_instance)
-        rs.UnselectAllObjects()
-        rs.SelectObjects(initial_objs)
-        print("Action annulée.")
 
 if __name__ == "__main__":
     rebuild_reciproque()
