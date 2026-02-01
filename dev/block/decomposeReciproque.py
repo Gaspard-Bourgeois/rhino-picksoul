749770bb1b0e6e36e706fa501a6243f2e19e347f
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-02-01 10:35:10 +0100

    Update decomposeReciproque.py

--- dev/block/decomposeReciproque.py
+++ dev/block/decomposeReciproque.py
@@ -1,8 +1,10 @@
 import rhinoscriptsyntax as rs
 
 def create_pose_block():
+    """Vérifie l'existence du bloc 'Pose' ou le crée avec un trièdre RVB."""
     if not rs.IsBlock("Pose"):
         items = []
+        # X = Rouge, Y = Vert, Z = Bleu
         items.append(rs.AddLine([0,0,0], [1,0,0]))
         rs.ObjectColor(items[-1], [255,0,0])
         items.append(rs.AddLine([0,0,0], [0,1,0]))
@@ -13,44 +15,70 @@ def create_pose_block():
     return "Pose"
 
 def decompose_reciproque():
-    # Support de la présélection
-    block_id = rs.GetObject("Sélectionnez une instance de bloc", rs.filter.instance, preselect=True)
-    if not block_id: return
+    # Support de la sélection multiple (objets de tout type)
+    object_ids = rs.GetObjects("Sélectionnez les blocs ou objets à décomposer", preselect=True)
+    if not object_ids: return
 
-    block_name = rs.BlockInstanceName(block_id)
-    block_xform = rs.BlockInstanceXform(block_id)
+    all_results = []
     
-    # Exploser le bloc
-    exploded_objects = rs.ExplodeBlockInstance(block_id)
+    rs.EnableRedraw(False)
     
-    origin_obj = None
-    # On cherche un objet qui a la même transformation (situé à l'origine 0,0,0 du bloc original)
-    # Dans Rhino, après explosion, on vérifie la position relative
-    for obj in exploded_objects:
-        # Si c'est une instance (sous-bloc), on peut comparer les xform
-        if rs.IsBlockInstance(obj):
-            if rs.BlockInstanceXform(obj) == block_xform:
-                origin_obj = obj
-                break
-    
-    # Si pas d'objet trouvé, on ajoute le bloc Pose
-    if not origin_obj:
-        create_pose_block()
-        origin_obj = rs.InsertBlock("Pose", [0,0,0])
-        rs.TransformObject(origin_obj, block_xform)
-        exploded_objects.append(origin_obj)
+    for obj_id in object_ids:
+        if rs.IsBlockInstance(obj_id):
+            # --- TRAITEMENT DES BLOCS ---
+            block_name = rs.BlockInstanceName(obj_id)
+            block_xform = rs.BlockInstanceXform(obj_id)
+            # Point d'insertion dans le repère Monde
+            insert_pt = rs.BlockInstanceInsertPoint(obj_id)
+            
+            # Exploser l'instance
+            exploded_items = rs.ExplodeBlockInstance(obj_id)
+            
+            origin_obj = None
+            # Recherche d'un sous-bloc situé exactement à l'origine du bloc parent
+            for item in exploded_items:
+                if rs.IsBlockInstance(item):
+                    # On compare le point d'insertion du sous-objet avec celui du parent
+                    if rs.BlockInstanceInsertPoint(item) == insert_pt:
+                        origin_obj = item
+                        break
+            
+            # Si aucune "Pose" ou objet d'origine n'est trouvé, on en crée un
+            if not origin_obj:
+                create_pose_block()
+                origin_obj = rs.InsertBlock("Pose", [0,0,0])
+                rs.TransformObject(origin_obj, block_xform)
+                exploded_items.append(origin_obj)
 
-    # Ajouter la clé
-    rs.SetUserText(origin_obj, "OriginalBlockName", block_name)
-    
-    # Grouper
-    group_name = rs.AddGroup()
-    rs.AddObjectsToGroup(exploded_objects, group_name)
-    
-    # Sélectionner les objets à la fin
+            # Ajout de la clé UserText
+            rs.SetUserText(origin_obj, "OriginalBlockName", block_name)
+            
+            # Création d'un groupe pour les composants du bloc explosé
+            group_name = rs.AddGroup()
+            rs.AddObjectsToGroup(exploded_items, group_name)
+            all_results.extend(exploded_items)
+            
+        else:
+            # --- TRAITEMENT DES AUTRES OBJETS (Explode standard) ---
+            # rs.ExplodeObjects décompose les courbes, polysurfaces, etc.
+            exploded_geometry = rs.ExplodeObjects(obj_id, delete_input=True)
+            
+            if exploded_geometry:
+                # Si l'objet a pu être décomposé, on groupe le résultat
+                group_name = rs.AddGroup()
+                rs.AddObjectsToGroup(exploded_geometry, group_name)
+                all_results.extend(exploded_geometry)
+            else:
+                # Si l'objet ne peut pas être explosé (ex: une ligne simple), on le garde
+                all_results.append(obj_id)
+
+    # --- FINALISATION ---
     rs.UnselectAllObjects()
-    rs.SelectObjects(exploded_objects)
-    print("Bloc décomposé. Objet origine : {}".format(origin_obj))
+    if all_results:
+        rs.SelectObjects(all_results)
+        
+    rs.EnableRedraw(True)
+    print("Traitement terminé sur {} objet(s) source(s).".format(len(object_ids)))
 
 if __name__ == "__main__":
     decompose_reciproque()
