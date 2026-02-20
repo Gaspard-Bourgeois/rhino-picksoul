8baa89c39b487dbc78dc3607e7151b8062cc45db
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-02-20 13:43:57 +0100

    Update decomposeReciproque.py

--- dev/block/decomposeReciproque.py
+++ dev/block/decomposeReciproque.py
@@ -17,23 +17,60 @@ def create_pose_block():
         rs.EnableRedraw(True)
     return "Pose"
 
-def explode_any(obj_id):
-    # TODO: fonction à remplacer par une fonction qui n'explose que les instances
-    """
-    Fonction utilitaire pour exploser n'importe quel type d'objet.
-    Remplace le 'rs.ExplodeObjects' inexistant.
-    """
-    if rs.IsPolysurface(obj_id):
-        return rs.ExplodePolysurfaces(obj_id, delete_input=True)
-    elif rs.IsCurve(obj_id) and not rs.IsLine(obj_id):
-        return rs.ExplodeCurves(obj_id, delete_input=True)
-    elif rs.IsMesh(obj_id):
-        return rs.ExplodeMesh(obj_id, delete_input=True)
+def explode_instance_only(obj_id):
+    """Explose l'objet uniquement s'il s'agit d'une instance de bloc."""
+    if rs.IsBlockInstance(obj_id):
+        return rs.ExplodeBlockInstance(obj_id)
     return None
+
+def get_next_instance_index(block_name):
+    """
+    Trouve l'indice Y unique pour un block_name donné dans le document
+    en scannant les UserTexts existants.
+    """
+    max_index = 0
+    # On cherche dans tous les objets du document ayant du UserText
+    all_objs = rs.AllObjects()
+    for obj in all_objs:
+        keys = rs.GetUserText(obj)
+        if keys:
+            for key in keys:
+                if key.startswith("BlockNameLevel_"):
+                    value = rs.GetUserText(obj, key)
+                    if value and "#" in value:
+                        name_part, index_part = value.split("#")
+                        if name_part == block_name:
+                            try:
+                                idx = int(index_part)
+                                if idx > max_index: max_index = idx
+                            except ValueError:
+                                continue
+    return max_index + 1
+
+def get_current_hierarchy_info(obj_id):
+    """
+    Récupère le niveau d'imbrication actuel à partir des UserTexts.
+    Retourne le niveau suivant (X) et les dictionnaires de textes existants.
+    """
+    keys = rs.GetUserText(obj_id)
+    max_level = -1
+    existing_data = {}
+    
+    if keys:
+        for key in keys:
+            if key.startswith("BlockNameLevel_"):
+                try:
+                    lvl = int(key.split("_")[-1])
+                    if lvl > max_level: max_level = lvl
+                    existing_data[key] = rs.GetUserText(obj_id, key)
+                except ValueError:
+                    continue
+    
+    return max_level + 1, existing_data
 
 def decompose_reciproque():
     # Sélection multiple
-    object_ids = rs.GetObjects("Sélectionnez les objets ou blocs à décomposer", preselect=True)
+    object_ids = rs.GetObjects("Sélectionnez les blocs à décomposer", preselect=True)
     if not object_ids: return
 
     all_results = []
@@ -46,57 +83,46 @@ def decompose_reciproque():
         if rs.IsBlockInstance(obj_id):
             block_name = rs.BlockInstanceName(obj_id)
             block_xform = rs.BlockInstanceXform(obj_id)
-            insert_pt = rs.BlockInstanceInsertPoint(obj_id)
             
-            # Explosion du bloc (un seul niveau)
-            exploded_items = list(rs.ExplodeBlockInstance(obj_id))
+            # 1. Récupération de la hiérarchie actuelle
+            next_level, hierarchy_history = get_current_hierarchy_info(obj_id)
             
-            origin_obj = None
-            # On cherche si un sous-bloc "Pose" est déjà présent à l'origine
-            # TODO : on insert dans tous les cas un bloc pose à l'origine
-            for item in exploded_items:
-                if rs.IsBlockInstance(item):
-                    if rs.BlockInstanceInsertPoint(item) == insert_pt:
-                        origin_obj = item
-                        break
+            # 2. Calcul de l'indice unique Y pour ce nom de bloc
+            instance_index = get_next_instance_index(block_name)
             
-            # Si pas de Pose trouvée, on l'insère
-            if not origin_obj:
-                origin_obj = rs.InsertBlock("Pose", [0,0,0])
-                rs.TransformObject(origin_obj, block_xform)
-                exploded_items.append(origin_obj)
-
-            # Marquage avec le nom d'origine
-            # TODO : Remplacer par un usertext du type BlocNameLevel_X = block_name avec X le niveau d'imbrication actuel
-            rs.SetUserText(origin_obj, "OriginalBlockName", block_name)
+            # 3. Explosion (un seul niveau)
+            exploded_items = explode_instance_only(obj_id)
+            if not exploded_items: continue
             
-            # Groupement du résultat
-            # TODO: on récupère le niveau hiérarchique actuelle des instances à décomposer à partir des usertexts du type BlocNameLevel_X = block_name#Y, X le niveau d'imbrication de bloc, Y l'indice de l'instance de bloc
-            # TODO: l'indice de l'instance décomposée est unique par block_name dans le document, tous les groupes de la même instances décomposé ont un indice d'instance différent
-            # TODO: on ne groupe plus les résultats
-            group = rs.AddGroup()
-            rs.AddObjectsToGroup(exploded_items, group)
+            # 4. Insertion du bloc Pose à l'origine locale transformée
+            origin_obj = rs.InsertBlock("Pose", [0,0,0])
+            rs.TransformObject(origin_obj, block_xform)
+            
+            # 5. Marquage UserText
+            # On reporte l'historique des niveaux parents
+            for key, val in hierarchy_history.items():
+                rs.SetUserText(origin_obj, key, val)
+            
+            # On ajoute le niveau actuel : BlockNameLevel_X = block_name#Y
+            new_key = "BlockNameLevel_{}".format(next_level)
+            new_value = "{}#{}".format(block_name, instance_index)
+            rs.SetUserText(origin_obj, new_key, new_value)
+            
             all_results.extend(exploded_items)
+            all_results.append(origin_obj)
 
-        # --- CAS 2 : C'EST UN OBJET GÉOMÉTRIQUE (Polysurface, Courbe, etc.) ---
-        #TODO : on le garde
+        # --- CAS 2 : OBJET GÉOMÉTRIQUE ---
         else:
-            exploded_geo = explode_any(obj_id)
-            if exploded_geo:
-                group = rs.AddGroup()
-                rs.AddObjectsToGroup(exploded_geo, group)
-                all_results.extend(exploded_geo)
-            else:
-                # Si l'objet n'est pas explosable (ex: une ligne seule), on le garde
-                all_results.append(obj_id)
+            # On le garde tel quel
+            all_results.append(obj_id)
 
-    # Finalisation
+    # Finalisation (pas de groupe selon les instructions)
     rs.UnselectAllObjects()
     if all_results:
         rs.SelectObjects(all_results)
     
     rs.EnableRedraw(True)
-    print("Décomposition terminée pour {} objet(s).".format(len(object_ids)))
+    print("Décomposition terminée. {} objets traités.".format(len(all_results)))
 
 if __name__ == "__main__":
     decompose_reciproque()
