4accfed2a28187c581cb8a01a5e28685f1b84a5d
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-02-20 14:00:28 +0100

    Update decomposeReciproque.py

--- dev/block/decomposeReciproque.py
+++ dev/block/decomposeReciproque.py
@@ -6,7 +6,6 @@ def create_pose_block():
     if not rs.IsBlock("Pose"):
         rs.EnableRedraw(False)
         items = []
-        # Axes X (Rouge), Y (Vert), Z (Bleu)
         items.append(rs.AddLine([0,0,0], [1,0,0]))
         rs.ObjectColor(items[-1], [255,0,0])
         items.append(rs.AddLine([0,0,0], [0,1,0]))
@@ -17,19 +16,9 @@ def create_pose_block():
         rs.EnableRedraw(True)
     return "Pose"
 
-def explode_instance_only(obj_id):
-    """Explose l'objet uniquement s'il s'agit d'une instance de bloc."""
-    if rs.IsBlockInstance(obj_id):
-        return rs.ExplodeBlockInstance(obj_id)
-    return None
-
 def get_next_instance_index(block_name):
-    """
-    Trouve l'indice Y unique pour un block_name donné dans le document
-    en scannant les UserTexts existants.
-    """
+    """Trouve l'indice Y unique pour un block_name donné dans le document."""
     max_index = 0
-    # On cherche dans tous les objets du document ayant du UserText
     all_objs = rs.AllObjects()
     for obj in all_objs:
         keys = rs.GetUserText(obj)
@@ -43,19 +32,14 @@ def get_next_instance_index(block_name):
                             try:
                                 idx = int(index_part)
                                 if idx > max_index: max_index = idx
-                            except ValueError:
-                                continue
+                            except: continue
     return max_index + 1
 
 def get_current_hierarchy_info(obj_id):
-    """
-    Récupère le niveau d'imbrication actuel à partir des UserTexts.
-    Retourne le niveau suivant (X) et les dictionnaires de textes existants.
-    """
+    """Récupère le niveau d'imbrication (X) et l'historique des UserTexts."""
     keys = rs.GetUserText(obj_id)
     max_level = -1
     existing_data = {}
-    
     if keys:
         for key in keys:
             if key.startswith("BlockNameLevel_"):
@@ -63,66 +47,61 @@ def get_current_hierarchy_info(obj_id):
                     lvl = int(key.split("_")[-1])
                     if lvl > max_level: max_level = lvl
                     existing_data[key] = rs.GetUserText(obj_id, key)
-                except ValueError:
-                    continue
-    
+                except: continue
     return max_level + 1, existing_data
 
 def decompose_reciproque():
-    # Sélection multiple
     object_ids = rs.GetObjects("Sélectionnez les blocs à décomposer", preselect=True)
     if not object_ids: return
 
     all_results = []
     create_pose_block()
-    
     rs.EnableRedraw(False)
     
     for obj_id in object_ids:
-        # --- CAS 1 : C'EST UN BLOC ---
+        # --- CAS 1 : INSTANCE DE BLOC ---
         if rs.IsBlockInstance(obj_id):
             block_name = rs.BlockInstanceName(obj_id)
             block_xform = rs.BlockInstanceXform(obj_id)
             
-            # 1. Récupération de la hiérarchie actuelle
+            # Récupération hiérarchie et calcul de l'indice unique
             next_level, hierarchy_history = get_current_hierarchy_info(obj_id)
-            
-            # 2. Calcul de l'indice unique Y pour ce nom de bloc
             instance_index = get_next_instance_index(block_name)
             
-            # 3. Explosion (un seul niveau)
-            exploded_items = explode_instance_only(obj_id)
-            if not exploded_items: continue
+            # Explosion
+            exploded_items = rs.ExplodeBlockInstance(obj_id)
+            if not exploded_items: exploded_items = []
             
-            # 4. Insertion du bloc Pose à l'origine locale transformée
-            origin_obj = rs.InsertBlock("Pose", [0,0,0])
-            rs.TransformObject(origin_obj, block_xform)
+            # Création et ajout du bloc Pose
+            pose_id = rs.InsertBlock("Pose", [0,0,0])
+            rs.TransformObject(pose_id, block_xform)
             
-            # 5. Marquage UserText
-            # On reporte l'historique des niveaux parents
-            for key, val in hierarchy_history.items():
-                rs.SetUserText(origin_obj, key, val)
+            # Liste complète des objets à marquer (géométries + Pose)
+            targets = list(exploded_items) + [pose_id]
             
-            # On ajoute le niveau actuel : BlockNameLevel_X = block_name#Y
-            new_key = "BlockNameLevel_{}".format(next_level)
-            new_value = "{}#{}".format(block_name, instance_index)
-            rs.SetUserText(origin_obj, new_key, new_value)
+            # Marquage de TOUS les objets décomposés
+            for item in targets:
+                # 1. On recopie l'historique parent
+                for key, val in hierarchy_history.items():
+                    rs.SetUserText(item, key, val)
+                
+                # 2. On ajoute le niveau actuel : BlocNameLevel_X = Nom#Y
+                new_key = "BlockNameLevel_{}".format(next_level)
+                new_value = "{}#{}".format(block_name, instance_index)
+                rs.SetUserText(item, new_key, new_value)
             
-            all_results.extend(exploded_items)
-            all_results.append(origin_obj)
+            all_results.extend(targets)
 
-        # --- CAS 2 : OBJET GÉOMÉTRIQUE ---
+        # --- CAS 2 : GÉOMÉTRIE SIMPLE ---
         else:
-            # On le garde tel quel
             all_results.append(obj_id)
 
-    # Finalisation (pas de groupe selon les instructions)
     rs.UnselectAllObjects()
     if all_results:
         rs.SelectObjects(all_results)
     
     rs.EnableRedraw(True)
-    print("Décomposition terminée. {} objets traités.".format(len(all_results)))
+    print("Décomposition terminée : {} objets marqués.".format(len(all_results)))
 
 if __name__ == "__main__":
     decompose_reciproque()
