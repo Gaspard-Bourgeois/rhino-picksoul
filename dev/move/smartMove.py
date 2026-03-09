25dfb06a7ed905dc45a07508fb6322c479cb941d
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-03-09 14:42:29 +0100

    Update smartMove.py

--- dev/move/smartMove.py
+++ dev/move/smartMove.py
@@ -29,21 +29,25 @@ def getWithOption(_message):
         rc.append(objref.ObjectId)
     _i = boolOption.CurrentValue
     go.Dispose()
-
+    
     return rc, _i
 
 def getOriginDestWithOption(_message):
     """
-    Demande à l'utilisateur de sélectionner un objet ou de cliquer sur l'option 'UseCPlane'.
-    Renvoie le type de sélection ("object", "cplane" ou None) et les données associées.
+    Demande une sélection unique. Désélectionne les objets précédents 
+    pour éviter que l'objet 'origine' ne soit repris pour la 'destination'.
     """
+    # On vide la sélection actuelle pour forcer un nouveau clic
+    rs.UnselectAllObjects()
+    
     go = Rhino.Input.Custom.GetObject()
     go.SetCommandPrompt(_message)
     go.SubObjectSelect = False
-    go.GroupSelect = True
+    go.GroupSelect = False
     go.AcceptNothing(True)
+    go.DisablePreSelect() # Force l'utilisateur à cliquer à ce moment précis
     
-    # Filtre géométrique équivalent à : rs.filter.instance + rs.filter.surface + rs.filter.polysurface + rs.filter.extrusion + rs.filter.curve
+    # Filtres géométriques
     go.GeometryFilter = (Rhino.DocObjects.ObjectType.InstanceReference | 
                          Rhino.DocObjects.ObjectType.Surface | 
                          Rhino.DocObjects.ObjectType.Brep | 
@@ -52,35 +56,34 @@ def getOriginDestWithOption(_message):
     
     go.AddOption("UseCPlane")
     
-    while True:
-        get_rc = go.Get()
-        if get_rc == Rhino.Input.GetResult.Object:
-            obj_ref = go.Object(0)
-            obj_id = obj_ref.ObjectId
-            pt = obj_ref.SelectionPoint()
-            go.Dispose()
-            # On recrée une structure similaire à ce que renvoie rs.GetObjectEx()
-            # pour rester compatible avec la fonction getClickedPlaneFromObjectEx
-            return "object", (obj_id, False, 0, pt, rs.CurrentView())
+    get_rc = go.Get()
+    
+    if get_rc == Rhino.Input.GetResult.Object:
+        obj_ref = go.Object(0)
+        obj_id = obj_ref.ObjectId
+        pt = obj_ref.SelectionPoint()
+        res = "object", (obj_id, False, 0, pt, rs.CurrentView())
+        go.Dispose()
+        return res
             
-        elif get_rc == Rhino.Input.GetResult.Option:
-            go.Dispose()
-            return "cplane", None
+    elif get_rc == Rhino.Input.GetResult.Option:
+        go.Dispose()
+        return "cplane", None
             
-        elif get_rc == Rhino.Input.GetResult.Cancel or get_rc == Rhino.Input.GetResult.Nothing:
-            go.Dispose()
-            return None, None
+    elif get_rc == Rhino.Input.GetResult.Nothing:
+        go.Dispose()
+        return "world", None
+        
+    go.Dispose()
+    return None, None
 
 def getClickedPlaneFromObjectEx(obj):
     objId = obj[0]
     objType = rs.ObjectType(objId)
-    # print(objType)
     if rs.IsBlockInstance(objId):
         blockTransform = rs.BlockInstanceXform(objId)
-        print("Is a Block")
         plane = rs.PlaneTransform(rs.WorldXYPlane(), blockTransform)
     elif objType == rs.filter.curve:
-        print("Is a Curve")
         moussePoint = obj[3]
         param = rs.CurveClosestPoint(objId, moussePoint)
         plane = rs.CurveFrame(objId, param)
@@ -90,58 +93,39 @@ def getClickedPlaneFromObjectEx(obj):
         pt = bcp[0]
         normal = - bcp[3]
         type = bcp[2][0]
-        # print(bcp)
         if type == 3:
-            print("Is a brep face")
             faceIdx = bcp[2][1]
             rs.EnableRedraw(False)
             faces = rs.ExplodePolysurfaces(objId)
-            # print(faces)
             face = faces[faceIdx] if faces else objId
             u, v = rs.SurfaceClosestPoint(face, pt)
             plane = rs.SurfaceFrame(face, [u, v])
             rs.DeleteObjects(faces)
             rs.EnableRedraw(True)
         else:
-            print("Is a brep element")
             plane = rs.PlaneFromNormal(pt, normal)
-                    
     return plane
 
 def pointBarycenter(points):
-    x = 0
-    y = 0
-    z = 0
-    for point in points:
-        x += point[0]
-        y += point[1]
-        z += point[2]
+    x = y = z = 0
+    for pt in points:
+        x += pt[0]; y += pt[1]; z += pt[2]
     return [x/len(points), y/len(points), z/len(points)]
 
 def getPlaneFromObject(objId):
     objType = rs.ObjectType(objId)
-    # print(objType)
     if rs.IsBlockInstance(objId):
         blockTransform = rs.BlockInstanceXform(objId)
-        print("Is a Block")
-        plane = rs.PlaneTransform(rs.WorldXYPlane(), blockTransform)
+        return rs.PlaneTransform(rs.WorldXYPlane(), blockTransform)
     elif objType == rs.filter.curve:
-        print("Is a Curve")
-        param = 0
-        plane = rs.CurveFrame(objId, param)
+        return rs.CurveFrame(objId, 0)
     elif objType == rs.filter.surface:
-        print("Is a Surface")
-        param = [0, 0]
-        plane = rs.SurfaceFrame(objId, param)
+        return rs.SurfaceFrame(objId, [0, 0])
     else:
-        print("Is an element")
         corners = rs.BoundingBox(objId)
         pt = pointBarycenter(corners)
-        plane = rs.MovePlane(rs.WorldXYPlane(), pt)
-    return plane
+        return rs.MovePlane(rs.WorldXYPlane(), pt)
 
-# RunCommand is the called when the user enters the command name in Rhino.
-# The command name is defined by the filname minus "_cmd.py"
 def RunCommand( is_interactive ):
     objs = rs.SelectedObjects()
     is_relative = False
@@ -150,51 +134,44 @@ def RunCommand( is_interactive ):
         objs, is_relative = getWithOption("Select objects to translate")
         
     if objs: 
-        # SÉLECTION ORIGINE
-        resTypeFrom, objFrom = getOriginDestWithOption("Select origin")
-        
+        # ÉTAPE 1 : ORIGINE
+        resTypeFrom, objFrom = getOriginDestWithOption("Select origin (Enter for WorldXY)")
         if resTypeFrom == "object":
             planeFrom = getClickedPlaneFromObjectEx(objFrom)
         elif resTypeFrom == "cplane":
-            print("Is the CPlane")
             planeFrom = rs.ViewCPlane()
-        else:
-            print("Is the WorldXYPlane")
+        elif resTypeFrom == "world":
             planeFrom = rs.WorldXYPlane()
+        else: return # Annulé
         
-        if planeFrom:
-            # SÉLECTION DESTINATION
-            resTypeDest, objDest = getOriginDestWithOption("Select destination")
-            
-            if resTypeDest == "object":
-                planeDest = getClickedPlaneFromObjectEx(objDest)
-                planeDest = planeDest if rs.IsBlockInstance(objDest[0]) else rs.RotatePlane(planeDest, 180, planeDest.XAxis)
-            elif resTypeDest == "cplane":
-                print("Is the CPlane")
-                planeDest = rs.ViewCPlane()
-            else:
-                print("Is the WorldXYPlane")
-                planeDest = rs.WorldXYPlane()
+        # ÉTAPE 2 : DESTINATION
+        resTypeDest, objDest = getOriginDestWithOption("Select destination (Enter for WorldXY)")
+        if resTypeDest == "object":
+            planeDest = getClickedPlaneFromObjectEx(objDest)
+            # Rotation 180 si ce n'est pas un bloc pour orienter vers l'extérieur
+            if not rs.IsBlockInstance(objDest[0]):
+                planeDest = rs.RotatePlane(planeDest, 180, planeDest.XAxis)
+        elif resTypeDest == "cplane":
+            planeDest = rs.ViewCPlane()
+        elif resTypeDest == "world":
+            planeDest = rs.WorldXYPlane()
+        else: return # Annulé
                 
-            if planeDest:
-                if is_relative:
-                    for obj in objs:
-                        planeObj = getPlaneFromObject(obj)
-                        planeXY = rs.WorldXYPlane()
-
-                        relative_xform = rs.XformRotation1(planeXY, planeObj)
-                        relative_planeDest = rs.PlaneTransform(planeDest, relative_xform)
-                        
-                        xform = rs.XformRotation1(planeObj, relative_planeDest)
-                        rs.TransformObject(obj, xform)
-                else:
-                    xform = rs.XformRotation1(planeFrom, planeDest)
-                    rs.TransformObjects(objs, xform)
+        # TRANSFORMATION
+        if planeFrom and planeDest:
+            if is_relative:
+                for obj in objs:
+                    planeObj = getPlaneFromObject(obj)
+                    xform = rs.XformRotation1(planeObj, rs.PlaneTransform(planeDest, rs.XformRotation1(rs.WorldXYPlane(), planeObj)))
+                    rs.TransformObject(obj, xform)
+            else:
+                xform = rs.XformRotation1(planeFrom, planeDest)
+                rs.TransformObjects(objs, xform)
                     
-                rs.SelectObjects(objs)
-                print("{} objets oriented".format(len(objs)))
-                return 0
+            rs.SelectObjects(objs)
+            print("{} objets orientés".format(len(objs)))
 
     return 0
-    
-RunCommand(True)
+
+if __name__ == "__main__":
+    RunCommand(True)
