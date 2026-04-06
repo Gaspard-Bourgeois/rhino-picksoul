460f3eb754763b901a025966ec65ac2b3fbe56b9
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-04-06 12:12:17 +0200

    Update openPluginFolder.py

--- dev/utilities/openPluginFolder.py
+++ dev/utilities/openPluginFolder.py
@@ -1,10 +1,10 @@
 """
 Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
-Version: 1.0
-Date: 22/12/25
+Version: 2.0
+Date: 06/04/2026
 """
 import os
 import rhinoscriptsyntax as rs
-path = "%AppData%/McNeel/Rhinoceros/7.0/Plug-ins/PythonPlugins/"
+path = "%AppData%/McNeel/Rhinoceros/8.0/Plug-ins/"
 expandpath = os.path.expandvars(path)
 rs.Command("_NoEcho -_OpenURL {}".format(expandpath))
