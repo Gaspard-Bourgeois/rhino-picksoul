569acec6b7b93377d1445239fbd55a2afe0f2446
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-05-12 14:48:17 +0200

    Update openPluginFolder.py

--- dev/utilities/openPluginFolder.py
+++ dev/utilities/openPluginFolder.py
@@ -1,10 +1,10 @@
 """
 Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
 Version: 2.0
-Date: 06/04/2026
+Date: 12/05/2026
 """
 import os
 import rhinoscriptsyntax as rs
-path = "%AppData%/McNeel/Rhinoceros/8.0/Plug-ins/"
+path = "%AppData%/McNeel/Rhinoceros/7.0/Plug-ins/Rhino Picksoul (4a97e0e1-48sz-s842-5s58-d4fs5sd541fs)/"
 expandpath = os.path.expandvars(path)
 rs.Command("_NoEcho -_OpenURL {}".format(expandpath))
