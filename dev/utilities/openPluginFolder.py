a3d15c0acf0965646bdc1c332c88ddadbcc393b4
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-05-12 14:50:36 +0200

    Update openPluginFolder.py

--- dev/utilities/openPluginFolder.py
+++ dev/utilities/openPluginFolder.py
@@ -7,4 +7,4 @@ import os
 import rhinoscriptsyntax as rs
 path = "%AppData%/McNeel/Rhinoceros/7.0/Plug-ins/Rhino Picksoul (4a97e0e1-48sz-s842-5s58-d4fs5sd541fs)/"
 expandpath = os.path.expandvars(path)
-rs.Command("_NoEcho -_OpenURL {}".format(expandpath))
+rs.Command('_NoEcho -_OpenURL "{}"'.format(expandpath))
