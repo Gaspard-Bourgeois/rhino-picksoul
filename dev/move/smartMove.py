8d803cc91f9a307bf306f74ac9ee173799d31ff0
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-05-27 14:30:09 +0200

    Update smartMove.py

--- dev/move/smartMove.py
+++ dev/move/smartMove.py
@@ -16,12 +16,13 @@ def getWithOption(_message):
 
     while True:
         get_rc = go.GetMultiple(1,0)
-        if get_rc==Rhino.Input.GetResult.Object:
+        if get_rc == Rhino.Input.GetResult.Object:
             break
-        if (go.CommandResult() != Rhino.Commands.Result.Success):
+        if go.CommandResult() != Rhino.Commands.Result.Success:
             break
         if get_rc == Rhino.Input.GetResult.Option:
             continue
+
     rc = []
     count = go.ObjectCount
     for i in range(count):
@@ -29,65 +30,68 @@ def getWithOption(_message):
         rc.append(objref.ObjectId)
     _i = boolOption.CurrentValue
     go.Dispose()
-    
     return rc, _i
+
 
 def getOriginDestWithOption(_message):
     rs.UnselectAllObjects()
-    
+
     go = Rhino.Input.Custom.GetObject()
     go.SetCommandPrompt(_message)
     go.SubObjectSelect = False
     go.GroupSelect = False
     go.AcceptNothing(True)
     go.DisablePreSelect()
-    
-    go.GeometryFilter = (Rhino.DocObjects.ObjectType.InstanceReference | 
-                         Rhino.DocObjects.ObjectType.Surface | 
-                         Rhino.DocObjects.ObjectType.Brep | 
-                         Rhino.DocObjects.ObjectType.Extrusion | 
+
+    go.GeometryFilter = (Rhino.DocObjects.ObjectType.InstanceReference |
+                         Rhino.DocObjects.ObjectType.Surface |
+                         Rhino.DocObjects.ObjectType.Brep |
+                         Rhino.DocObjects.ObjectType.Extrusion |
                          Rhino.DocObjects.ObjectType.Curve)
-    
+
     go.AddOption("UseCPlane")
-    
+
     get_rc = go.Get()
-    
+
     if get_rc == Rhino.Input.GetResult.Object:
         obj_ref = go.Object(0)
         obj_id = obj_ref.ObjectId
         pt = obj_ref.SelectionPoint()
-        res = "object", (obj_id, False, 0, pt, rs.CurrentView())
         go.Dispose()
-        return res
-            
+        return "object", (obj_id, False, 0, pt, rs.CurrentView())
+
     elif get_rc == Rhino.Input.GetResult.Option:
         go.Dispose()
         return "cplane", None
-            
+
     elif get_rc == Rhino.Input.GetResult.Nothing:
         go.Dispose()
         return "world", None
-        
+
     go.Dispose()
     return None, None
+
 
 def getClickedPlaneFromObjectEx(obj):
     objId = obj[0]
     objType = rs.ObjectType(objId)
+
     if rs.IsBlockInstance(objId):
         blockTransform = rs.BlockInstanceXform(objId)
         plane = rs.PlaneTransform(rs.WorldXYPlane(), blockTransform)
+
     elif objType == rs.filter.curve:
-        moussePoint = obj[3]
-        param = rs.CurveClosestPoint(objId, moussePoint)
+        mousePoint = obj[3]
+        param = rs.CurveClosestPoint(objId, mousePoint)
         plane = rs.CurveFrame(objId, param)
+
     else:
-        moussePoint = obj[3]
-        bcp = rs.BrepClosestPoint(objId, moussePoint)
+        mousePoint = obj[3]
+        bcp = rs.BrepClosestPoint(objId, mousePoint)
         pt = bcp[0]
         normal = -bcp[3]
-        type = bcp[2][0]
-        if type == 3:
+        faceType = bcp[2][0]
+        if faceType == 3:
             faceIdx = bcp[2][1]
             rs.EnableRedraw(False)
             faces = rs.ExplodePolysurfaces(objId)
@@ -98,13 +102,16 @@ def getClickedPlaneFromObjectEx(obj):
             rs.EnableRedraw(True)
         else:
             plane = rs.PlaneFromNormal(pt, normal)
+
     return plane
+
 
 def pointBarycenter(points):
     x = y = z = 0
     for pt in points:
         x += pt[0]; y += pt[1]; z += pt[2]
-    return [x/len(points), y/len(points), z/len(points)]
+    return [x / len(points), y / len(points), z / len(points)]
+
 
 def getPlaneFromObject(objId):
     objType = rs.ObjectType(objId)
@@ -120,13 +127,24 @@ def getPlaneFromObject(objId):
         pt = pointBarycenter(corners)
         return rs.MovePlane(rs.WorldXYPlane(), pt)
 
+
+def buildXform(planeFrom, planeDest):
+    """
+    Construit la transformation qui amène planeFrom sur planeDest.
+    = XformChangeBasis(planeDest, WorldXY) * XformChangeBasis(WorldXY, planeFrom)
+    """
+    xform_from_to_world = rs.XformChangeBasis(rs.WorldXYPlane(), planeFrom)
+    xform_world_to_dest = rs.XformChangeBasis(planeDest, rs.WorldXYPlane())
+    return rs.XformMultiply(xform_world_to_dest, xform_from_to_world)
+
+
 def RunCommand(is_interactive):
     objs = rs.SelectedObjects()
     is_relative = False
-    
+
     if not objs:
         objs, is_relative = getWithOption("Select objects to translate")
-        
+
     if objs:
         # ÉTAPE 1 : ORIGINE
         resTypeFrom, objFrom = getOriginDestWithOption("Select origin (Enter for WorldXY)")
@@ -155,25 +173,24 @@ def RunCommand(is_interactive):
         # TRANSFORMATION
         if planeFrom and planeDest:
             if is_relative:
-                # Transformation relative : on calcule le delta entre planeFrom et planeDest,
-                # puis on l'applique depuis le repère local de chaque objet
-                xform_delta = rs.XformChangeBasis(planeFrom, planeDest)
+                # Delta entre planeFrom et planeDest, appliqué dans le repère local de chaque objet
                 for obj in objs:
                     planeObj = getPlaneFromObject(obj)
-                    # On exprime le delta dans le repère local de l'objet
-                    xform_to_local = rs.XformChangeBasis(rs.WorldXYPlane(), planeObj)
-                    xform_to_world = rs.XformChangeBasis(planeObj, rs.WorldXYPlane())
-                    xform = rs.XformMultiply(xform_to_world, rs.XformMultiply(xform_delta, xform_to_local))
+                    xform_obj_to_world = rs.XformChangeBasis(planeObj, rs.WorldXYPlane())
+                    xform_world_to_obj = rs.XformChangeBasis(rs.WorldXYPlane(), planeObj)
+                    xform_delta = buildXform(planeFrom, planeDest)
+                    xform = rs.XformMultiply(xform_obj_to_world,
+                             rs.XformMultiply(xform_delta, xform_world_to_obj))
                     rs.TransformObject(obj, xform)
             else:
-                # Transformation absolue : changement de repère complet de planeFrom vers planeDest
-                xform = rs.XformChangeBasis(planeFrom, planeDest)
+                xform = buildXform(planeFrom, planeDest)
                 rs.TransformObjects(objs, xform)
 
             rs.SelectObjects(objs)
             print("{} objets orientés".format(len(objs)))
 
     return 0
+
 
 if __name__ == "__main__":
     RunCommand(True)
