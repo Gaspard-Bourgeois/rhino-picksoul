431dc95d6ade6c5ff738189d76e62e5e037c16d0
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-06-05 16:22:43 +0200

    Update decomposeReciproque.py

--- dev/block/decomposeReciproque.py
+++ dev/block/decomposeReciproque.py
@@ -52,3 +52,128 @@ def get_next_instance_index(base_name):
                                 if idx > max_index:
                                     max_index = idx
                         except ValueError:
+                            continue
+    return max_index + 1
+
+def get_current_hierarchy_info(obj_id):
+    """Récupère le niveau d'imbrication (X) et l'historique des UserTexts."""
+    keys = rs.GetUserText(obj_id)
+    max_level = -1
+    existing_data = {}
+    if keys:
+        for key in keys:
+            if key.startswith("BlockNameLevel_"):
+                try:
+                    lvl = int(key.split("_")[-1])
+                    if lvl > max_level:
+                        max_level = lvl
+                    existing_data[key] = rs.GetUserText(obj_id, key)
+                except ValueError:
+                    continue
+    return max_level + 1, existing_data
+
+def decompose_reciproque():
+    # --- 1. CONFIGURATION DE L'OUTIL DE SÉLECTION ---
+    go = Rhino.Input.Custom.GetObject()
+    go.SetCommandPrompt("Sélectionnez les blocs à décomposer")
+    go.EnablePreSelect(True, True)
+    go.EnablePostSelect(True)
+
+    # --- 2. GESTION DES RÉGLAGES PERSISTANTS ---
+    rhino_settings = Rhino.PlugIns.PlugIn.GetPluginSettings(Rhino.RhinoApp.CurrentRhinoId, False)
+    script_settings = rhino_settings.AddChild("DecomposeReciproqueSettings")
+    blocs_to_current_layer = script_settings.GetBool("BlocsToCurrentLayer", False)
+
+    opt_toggle = Rhino.Input.Custom.OptionToggle(blocs_to_current_layer, "Non", "Oui")
+    go.AddOptionToggle("BlocsToCurrentLayer", opt_toggle)
+
+    object_ids = []
+
+    # --- 3. BOUCLE DE SÉLECTION ET GESTION DE L'OPTION ---
+    while True:
+        res = go.GetMultiple(1, 0)
+
+        if res == Rhino.Input.GetResult.Option:
+            blocs_to_current_layer = opt_toggle.CurrentValue
+            script_settings.SetBool("BlocsToCurrentLayer", blocs_to_current_layer)
+            Rhino.PlugIns.PlugIn.SavePluginSettings(Rhino.RhinoApp.CurrentRhinoId)
+            continue
+
+        elif res == Rhino.Input.GetResult.Object:
+            object_ids = [obj.ObjectId for obj in go.Objects()]
+            break
+
+        else:
+            return
+
+    if not object_ids:
+        return
+
+    all_results = []
+    create_pose_block()
+    rs.EnableRedraw(False)
+
+    current_layer = rs.CurrentLayer()
+
+    # --- 4. TRAITEMENT DES OBJETS ---
+    for obj_id in object_ids:
+
+        # --- CAS 1 : INSTANCE DE BLOC ---
+        if rs.IsBlockInstance(obj_id):
+            block_name = rs.BlockInstanceName(obj_id)
+
+            # SECURITE : On ne décompose JAMAIS le bloc "Pose"
+            if block_name == "Pose":
+                all_results.append(obj_id)
+                continue
+
+            block_xform = rs.BlockInstanceXform(obj_id)
+
+            # Récupération hiérarchie
+            next_level, hierarchy_history = get_current_hierarchy_info(obj_id)
+
+            # --- CORRECTION CLEF ---
+            # On travaille toujours sur le nom racine (sans suffixe #i éventuel)
+            base_name = get_base_name(block_name)
+            instance_index = get_next_instance_index(base_name)
+            # La valeur stockée est toujours "racine#j"
+            new_value = "{}#{}".format(base_name, instance_index)
+
+            # Explosion
+            exploded_items = rs.ExplodeBlockInstance(obj_id)
+            if not exploded_items:
+                exploded_items = []
+
+            # Création et transformation du bloc Pose
+            pose_id = rs.InsertBlock("Pose", [0,0,0])
+            rs.TransformObject(pose_id, block_xform)
+
+            targets = list(exploded_items) + [pose_id]
+
+            for item in targets:
+                if blocs_to_current_layer:
+                    rs.ObjectLayer(item, current_layer)
+
+                # Recopie de l'historique parent
+                for key, val in hierarchy_history.items():
+                    rs.SetUserText(item, key, val)
+
+                # Ajout du niveau actuel avec la valeur normalisée
+                new_key = "BlockNameLevel_{}".format(next_level)
+                rs.SetUserText(item, new_key, new_value)
+
+            all_results.extend(targets)
+
+        # --- CAS 2 : GÉOMÉTRIE SIMPLE ---
+        else:
+            all_results.append(obj_id)
+
+    rs.UnselectAllObjects()
+    if all_results:
+        rs.SelectObjects(all_results)
+
+    rs.EnableRedraw(True)
+    print("Décomposition terminée : {} objets créés ou conservés.".format(len(all_results)))
+
+if __name__ == "__main__":
+    decompose_reciproque()
