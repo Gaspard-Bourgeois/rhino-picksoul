b76a7fac06b6a7b618728bb4617d9f44a0f5789f
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-06-16 14:09:19 +0200

    Update updateAnnotationStyle.py

--- dev/label/updateAnnotationStyle.py
+++ dev/label/updateAnnotationStyle.py
@@ -1,68 +1,69 @@
 """
 Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
-Version: 1.1
-Date: 16/06/2026
+Version: 1.0
+Date: 16/02/2026
 """
 # -*- coding: utf-8 -*-
 import rhinoscriptsyntax as rs
-import scriptcontext as sc
 
-def update_annotations():
+def update_annotations_with_options():
+    # 1. Récupérer les styles et le style actuel
     styles = rs.DimStyleNames()
     if not styles:
-        print(u"Aucun style d'annotation trouv\xe9.")
+        print("Aucun style d'annotation trouvé.")
         return
 
     current_default = rs.CurrentDimStyle()
+    
+    # Nettoyage des noms de styles pour l'affichage en options (Rhino n'aime pas les espaces dans les mots-clés)
+    # On crée un dictionnaire pour faire la correspondance si besoin, 
+    # mais rs.GetString accepte généralement les listes directes.
+    options = [s.replace(" ", "_") for s in styles if s != None]
 
-    # Lecture et nettoyage immédiat du paramètre éventuel
-    chosen_style = sc.sticky.pop("dimstyle_target", None)
+    # 2. Demander le style via la barre de commande
+    msg = "Style actuel : {}. Choisir le nouveau style".format(current_default)
+    res = rs.GetString(msg, current_default, options)
 
-    # Si aucun paramètre, demander à l'utilisateur
+    if res is None: return # Annulation
+    
+    # Retrouver le nom original du style (si on a remplacé les espaces par des underscores)
+    chosen_style = None
+    if res in styles:
+        chosen_style = res
+    else:
+        # Recherche de correspondance pour gérer les espaces/underscores
+        for s in styles:
+            if s.replace(" ", "_") == res or s == res:
+                chosen_style = s
+                break
+    
     if not chosen_style:
-        options = [s.replace(" ", "_") for s in styles if s is not None]
-        msg = u"Style actuel : " + current_default + u". Choisir le nouveau style"
-        res = rs.GetString(msg, current_default, options)
-        if res is None:
-            return
-
-        if res in styles:
-            chosen_style = res
-        else:
-            for s in styles:
-                if s is not None and (s.replace(" ", "_") == res or s == res):
-                    chosen_style = s
-                    break
-
-    if not chosen_style:
-        print(u"Style invalide.")
+        print("Style invalide.")
         return
 
-    undo_was_enabled = sc.doc.UndoRecordingEnabled
-    sc.doc.UndoRecordingEnabled = False
-
-    try:
-        selected_objs = rs.SelectedObjects()
-
-        if selected_objs:
-            rs.EnableRedraw(False)
-            anno_count = 0
-            for obj_id in selected_objs:
-                if rs.ObjectType(obj_id) == 512:
-                    rs.DimensionStyle(obj_id, chosen_style)
-                    anno_count += 1
-            rs.EnableRedraw(True)
-            if anno_count > 0:
-                print(u"Succ\xe8s : %d annotation(s) mise(s) \xe0 jour sur '%s'." % (anno_count, chosen_style))
-            else:
-                print(u"Aucune annotation trouv\xe9e dans la s\xe9lection.")
+    # 3. Traiter la sélection
+    selected_objs = rs.SelectedObjects()
+    
+    if selected_objs:
+        rs.EnableRedraw(False)
+        anno_count = 0
+        for obj_id in selected_objs:
+            # 512 = Code pour tous les objets d'annotation (Cotes, Textes, Leaders)
+            if rs.ObjectType(obj_id) == 512:
+                rs.DimensionStyle(obj_id, chosen_style)
+                anno_count += 1
+        rs.EnableRedraw(True)
+        
+        if anno_count > 0:
+            print("Succès : {} annotation(s) mise(s) à jour sur '{}'.".format(anno_count, chosen_style))
         else:
-            print(u"Aucune s\xe9lection. Modification du style par d\xe9faut uniquement.")
+            print("Aucune annotation trouvée dans la sélection.")
+    else:
+        print("Aucune sélection. Modification du style par défaut uniquement.")
 
-        rs.CurrentDimStyle(chosen_style)
-        print(u"Style par d\xe9faut d\xe9fini sur : " + chosen_style)
+    # 4. Définir comme style par défaut
+    rs.CurrentDimStyle(chosen_style)
+    print("Style par défaut défini sur : {}".format(chosen_style))
 
-    finally:
-        sc.doc.UndoRecordingEnabled = undo_was_enabled
-
-update_annotations()
+if __name__ == "__main__":
+    update_annotations_with_options()
