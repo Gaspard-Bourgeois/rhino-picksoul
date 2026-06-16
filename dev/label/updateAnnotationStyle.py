927baf0032f88c2688f2f7aa0f0ff484dc169f16
Gaspard BOURGEOIS <42674277+Gaspard-Bourgeois@users.noreply.github.com>
2026-06-16 11:28:53 +0200

    Update updateAnnotationStyle.py

--- dev/label/updateAnnotationStyle.py
+++ dev/label/updateAnnotationStyle.py
@@ -3,35 +3,13 @@ Author: Gaspard BOURGEOIS <gaspard.github.io@free.fr>
 Version: 1.1
 Date: 16/06/2026
 """
-# -*- coding: utf-8 -*-
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 
-def update_annotations_with_options():
-    styles = rs.DimStyleNames()
-    if not styles:
-        print("Aucun style d'annotation trouvé.")
-        return
-
-    current_default = rs.CurrentDimStyle()
-    options = [s.replace(" ", "_") for s in styles if s is not None]
-
-    msg = "Style actuel : {}. Choisir le nouveau style".format(current_default)
-    res = rs.GetString(msg, current_default, options)
-    if res is None:
-        return
-
-    chosen_style = None
-    if res in styles:
-        chosen_style = res
-    else:
-        for s in styles:
-            if s.replace(" ", "_") == res or s == res:
-                chosen_style = s
-                break
-
+def update_annotations():
+    chosen_style = sc.sticky.get("dimstyle_target", None)
     if not chosen_style:
-        print("Style invalide.")
+        print(u"Aucun style d\xe9fini.")
         return
 
     undo_was_enabled = sc.doc.UndoRecordingEnabled
@@ -39,7 +17,6 @@ def update_annotations_with_options():
 
     try:
         selected_objs = rs.SelectedObjects()
-
         if selected_objs:
             rs.EnableRedraw(False)
             anno_count = 0
@@ -48,19 +25,17 @@ def update_annotations_with_options():
                     rs.DimensionStyle(obj_id, chosen_style)
                     anno_count += 1
             rs.EnableRedraw(True)
-
             if anno_count > 0:
-                print(u"Succès : {} annotation(s) mise(s) à jour sur '{}'.".format(anno_count, chosen_style))
+                print(u"Succ\xe8s : %d annotation(s) mise(s) \xe0 jour sur '%s'." % (anno_count, chosen_style))
             else:
-                print(u"Aucune annotation trouvée dans la sélection.")
+                print(u"Aucune annotation trouv\xe9e dans la s\xe9lection.")
         else:
-            print(u"Aucune sélection. Modification du style par défaut uniquement.")
+            print(u"Aucune s\xe9lection. Modification du style par d\xe9faut uniquement.")
 
         rs.CurrentDimStyle(chosen_style)
-        print(u"Style par défaut défini sur : {}".format(chosen_style))
+        print(u"Style par d\xe9faut d\xe9fini sur : " + chosen_style)
 
     finally:
         sc.doc.UndoRecordingEnabled = undo_was_enabled
 
-if __name__ == "__main__":
-    update_annotations_with_options()
+update_annotations()
